825ffc568852a351f5943e54290f272523e94ead
Sebastien Lebreton <sebastien@lebreton.fr>
2016-10-14 10:59:07 -0700

    Cleanup Web

--- BdtWebServer/Runtime/BdtWebServer.cs
+++ BdtWebServer/Runtime/BdtWebServer.cs
@@ -1,4 +1,4 @@
-/* BoutDuTunnel Copyright (c)  2007-2013 Sebastien LEBRETON
+/* BoutDuTunnel Copyright (c) 2007-2016 Sebastien LEBRETON
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
@@ -19,146 +19,92 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
-#region " Inclusions "
 using System;
 using System.Globalization;
 using System.IO;
 using System.Web;
-
 using Bdt.Server.Service;
 using Bdt.Server.Runtime;
 using Bdt.Shared.Configuration;
 using Bdt.Shared.Logs;
 using Bdt.Shared.Runtime;
-#endregion
 
 namespace Bdt.WebServer.Runtime
 {
+	public class BdtWebServer : Program
+	{
+		private readonly HttpServerUtility _server;
 
-    /// -----------------------------------------------------------------------------
-    /// <summary>
-    /// Programme côté serveur du tunnel de communication
-    /// </summary>
-    /// -----------------------------------------------------------------------------
-    public class BdtWebServer : Program
-    {
+		public override string ConfigFile
+		{
+			get { return _server.MapPath(string.Format("App_Data" + Path.DirectorySeparatorChar + "{0}Cfg.xml", typeof(BdtServer).Assembly.GetName().Name)); }
+		}
 
-        #region " Attributs "
-	    readonly HttpServerUtility _server;
-        #endregion
+		public BdtWebServer(HttpServerUtility server)
+		{
+			_server = server;
+		}
 
-        #region " Proprietes "
-        public override string ConfigFile
-        {
-            get
-            {
-                return _server.MapPath(string.Format("App_Data" + Path.DirectorySeparatorChar + "{0}Cfg.xml", typeof(BdtServer).Assembly.GetName().Name));
-            }
-        }
-        #endregion
-        
-        #region " Méthodes "
-        /// -----------------------------------------------------------------------------
-        /// <summary>
-        /// Constructeur
-        /// </summary>
-        /// <param name="server">l'utilitaire serveur (mappage)</param>
-        /// -----------------------------------------------------------------------------
-        public BdtWebServer(HttpServerUtility server)
-        {
-            _server = server;
-        }
+		protected override void SetCulture(string name)
+		{
+			base.SetCulture(name);
+			if (!string.IsNullOrEmpty(name))
+				Server.Resources.Strings.Culture = new CultureInfo(name);
+		}
 
-        /// -----------------------------------------------------------------------------
-        /// <summary>
-        /// Fixe la culture courante
-        /// </summary>
-        /// <param name="name">le nom de la culture</param>
-        /// -----------------------------------------------------------------------------
-        protected override void SetCulture(String name)
-        {
-            base.SetCulture(name);
-	        if (!string.IsNullOrEmpty(name))
-		        Server.Resources.Strings.Culture = new CultureInfo(name);
-        }
+		public override void LoadConfiguration(string[] args)
+		{
+			Args = args;
 
-        /// -----------------------------------------------------------------------------
-        /// <summary>
-        /// Chargement des données de configuration
-        /// </summary>
-        /// <param name="args">Arguments de la ligne de commande</param>
-        /// -----------------------------------------------------------------------------
-        public override void LoadConfiguration(string[] args)
-        {
-            Args = args;
+			GlobalLogger = CreateLoggers();
+			Log(Shared.Resources.Strings.LOADING_CONFIGURATION, ESeverity.DEBUG);
+			var cfg = new SharedConfig(Configuration);
+			// unneeded in IIs web hosting model, see web.config
+			// m_protocol = GenericProtocol.GetInstance(cfg);
+			SetCulture(cfg.ServiceCulture);
+		}
 
-            GlobalLogger = CreateLoggers();
-            Log(Shared.Resources.Strings.LOADING_CONFIGURATION, ESeverity.DEBUG);
-            var cfg = new SharedConfig(Configuration);
-            // unneeded in IIs web hosting model, see web.config
-            // m_protocol = GenericProtocol.GetInstance(cfg);
-            SetCulture(cfg.ServiceCulture);
-        }
+		public void Start()
+		{
+			LoadConfiguration(new String[] {});
 
-        /// -----------------------------------------------------------------------------
-        /// <summary>
-        /// Démarrage du serveur
-        /// </summary>
-        /// -----------------------------------------------------------------------------
-        public void Start()
-        {
-            LoadConfiguration(new String[] { });
+			Log(string.Format(Server.Resources.Strings.SERVER_TITLE, GetType().Assembly.GetName().Version.ToString(3)), ESeverity.INFO);
+			Log(FrameworkVersion(), ESeverity.INFO);
 
-            Log(string.Format(Server.Resources.Strings.SERVER_TITLE, GetType().Assembly.GetName().Version.ToString(3)), ESeverity.INFO);
-            Log(FrameworkVersion(), ESeverity.INFO);
+			Tunnel.Configuration = Configuration;
+			Tunnel.Logger = GlobalLogger;
 
-            Tunnel.Configuration = Configuration;
-            Tunnel.Logger = GlobalLogger;
+			// unneeded in IIs web hosting model, see web.config
+			// server.Protocol.ConfigureServer(typeof(Tunnel));
+			Log(Server.Resources.Strings.SERVER_STARTED, ESeverity.INFO);
+		}
 
-            // unneeded in IIs web hosting model, see web.config
-            // server.Protocol.ConfigureServer(typeof(Tunnel));
-            Log(Server.Resources.Strings.SERVER_STARTED, ESeverity.INFO);
-        }
+		protected override BaseLogger CreateLoggers()
+		{
+			var xmlConfig = new XMLConfig(ConfigFile, 1);
+			Configuration = new ConfigPackage();
+			Configuration.AddSource(xmlConfig);
 
-        /// -----------------------------------------------------------------------------
-        /// <summary>
-        /// Initialisation des loggers
-        /// </summary>
-        /// <returns>un MultiLogger lié à une source fichier et console</returns>
-        /// -----------------------------------------------------------------------------
-        protected override BaseLogger CreateLoggers()
-        {
-            var xmlConfig = new XMLConfig(ConfigFile, 1);
-            Configuration = new ConfigPackage();
-            Configuration.AddSource(xmlConfig);
+			// Map the path to the current Web Application
+			const string key = CfgFile + Shared.Configuration.BaseConfig.SourceItemAttribute + FileLogger.ConfigFilename;
+			var filename = xmlConfig.Value(key, null);
+			if ((filename != null) && (!Path.IsPathRooted(filename)))
+				xmlConfig.SetValue(key, _server.MapPath("App_Data" + Path.DirectorySeparatorChar + filename));
 
-            // Map the path to the current Web Application
-            const string key = CfgFile + Shared.Configuration.BaseConfig.SourceItemAttribute + FileLogger.ConfigFilename;
-            var filename = xmlConfig.Value(key, null);
-	        if ((filename != null) && (!Path.IsPathRooted(filename)))
-		        xmlConfig.SetValue(key, _server.MapPath("App_Data" + Path.DirectorySeparatorChar + filename));
+			var log = new MultiLogger();
+			ConsoleLogger = new ConsoleLogger(CfgConsole, Configuration);
+			FileLogger = new FileLogger(CfgFile, Configuration);
+			log.AddLogger(ConsoleLogger);
+			log.AddLogger(FileLogger);
 
-	        var log = new MultiLogger();
-            ConsoleLogger = new ConsoleLogger(CfgConsole, Configuration);
-            FileLogger = new FileLogger(CfgFile, Configuration);
-            log.AddLogger(ConsoleLogger);
-            log.AddLogger(FileLogger);
+			return log;
+		}
 
-            return log;
-        }
-
-        /// -----------------------------------------------------------------------------
-        /// <summary>
-        /// Arrêt du serveur
-        /// </summary>
-        /// -----------------------------------------------------------------------------
-        public void Stop()
-        {
-            Tunnel.DisableChecking();
-            Protocol.UnConfigureServer();
-            UnLoadConfiguration();
-        }
-        #endregion
-
-    }
+		public void Stop()
+		{
+			Tunnel.DisableChecking();
+			Protocol.UnConfigureServer();
+			UnLoadConfiguration();
+		}
+	}
 }
