60f07c9db305cb010a88075853acdb513855656e
Sebastien Lebreton <sebastien@lebreton.fr>
2021-05-03 14:50:01 -0700

    Update Copyright

--- Bdt.GuiClient/Forms/MainComponent.cs
+++ Bdt.GuiClient/Forms/MainComponent.cs
@@ -1,4 +1,4 @@
-﻿/* BoutDuTunnel Copyright (c) 2006-2019 Sebastien Lebreton
+﻿/* BoutDuTunnel Copyright (c) 2006-2021 Sebastien Lebreton
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
